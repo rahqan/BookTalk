a62645f7f7f6188cc464914029e95d850d5b0aa4
rahqan <rahqansayeed@gmail.com>
2024-08-25 00:04:16 +0530

    user1 can write and read hardcoder user 1, now trying to implement user login

---
+++ index.js
@@ -0,0 +1,276 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import env from "dotenv";
+
+
+
+
+
+
+
+
+
+const app = express();
+const port = 3000;
+env.config();
+
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  });
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
+
+
+
+
+app.get('/',async(req,res)=>{
+
+    let result=await(db.query("Select id,name,author from books"));
+    let books = result.rows;
+    // console.log(books);
+    res.render("index.ejs", { books: books });
+    // res.render("index.ejs");
+})
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+
+app.get("/logout", (req, res) => {
+    req.logout(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
+
+
+
+// Route to get replies for a specific discussion
+// ingternal ajax routye
+
+
+
+app.get('/get-replies', async (req, res) => {
+    let parentId = req.query.parent_id;
+    // console.log(parentId);
+    const discussId = req.query.discuss_id;
+
+    if (parentId === "null") {
+        parentId = null;
+    }
+    let result;
+
+    try {
+        // Query to get the replies for the given discussion ID and parent ID
+
+        if (parentId !== null) {
+            result = await db.query(
+                'SELECT * FROM replies WHERE parent_id = $1 AND discuss_id = $2',
+                [parentId, discussId]
+            );
+        } else {
+            result = await db.query(
+                'SELECT * FROM replies WHERE parent_id IS NULL AND discuss_id = $1',
+                [discussId]
+            );
+        }
+
+        // console.log()
+
+        // Send the replies as a JSON response
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error querying the database:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+
+app.post('/search',async(req,res)=>{
+
+    // console.log(req.body.query);
+    let bookName=req.body.query;
+    // get book data
+   let fromDb=await(db.query("Select * from books where name like '%' || $1 || '%';",[bookName]));
+    let bookData = fromDb.rows[0];
+    let bookId=bookData.id;
+    // get discussions using the bookId
+    fromDb=await(db.query("select * from discussions where book_id = ($1)",[bookId]));
+    let discussion=fromDb.rows;
+
+    // console.log(discussion);
+
+    res.render("book.ejs",{book:bookData,discussions:discussion});
+    // res.render("index.ejs");
+
+
+})
+
+
+
+app.post('/post-discuss', async (req, res) => {
+
+
+    // if (req.query.discuss_id) {
+    //     const { discuss_id, text } = req.query; 
+    // }
+    // else{
+
+    // }
+    
+    const { book_id, text } = req.query; // Use req.body for data from POST requests if sent in body
+    const userId = 1; // Replace with the actual user ID from the session
+
+//     if (req.query.discuss_id) {
+//         try {
+//             await db.query(
+//                 'INSERT INTO replies (discuss_id,user_id, text) VALUES ($1, $2, $3)',
+//                 [discuss_id, userId, text]
+//             );
+//             res.status(200).send('Reply posted successfully');
+//         } catch (error) {
+//             console.error('Error posting reply:', error);
+//             res.status(500).send('Failed to post reply');
+//         }
+//     }
+
+// else{
+
+    try {
+        await db.query(
+            'INSERT INTO discussions (user_id, book_id, text) VALUES ($1, $2, $3)',
+            [userId, book_id, text]
+        );
+        res.status(200).send('Reply posted successfully');
+    } catch (error) {
+        console.error('Error posting reply:', error);
+        res.status(500).send('Failed to post reply');
+    }
+// }
+});
+
+
+
+
+app.post('/post-reply', async (req, res) => {
+    const { discuss_id, parent_id, text } = req.query; // Use req.body for POST data
+    const userId = 1; // Replace with the actual user ID from the session
+    console.log(parent_id);
+    try {
+        if (parent_id !== 'null') {
+            console.log('fuck this');
+            await db.query(
+                'INSERT INTO replies (discuss_id, parent_id, text, user_id) VALUES ($1, $2, $3, $4)',
+                [discuss_id, parent_id, text, userId]
+            );
+        } else {
+            
+             console.log('notme son');
+             await db.query(
+                'INSERT INTO replies (discuss_id, text, user_id) VALUES ($1, $2, $3)',
+                [discuss_id, text, userId]
+            );
+        }
+        res.status(200).send('Reply posted successfully');
+    } catch (error) {
+        // console.log('pewpewpew');
+        console.error('Error posting reply:', error);
+        res.status(500).send('Failed to post reply');
+    }
+});
+
+
+
+// app.get('/get-replyreplies', async (req, res) => {
+//     // const discussId = req.query.discuss_id;
+//     const parentId = req.query.parent_id;
+//     // console.log(discussId);
+//     try {
+//         // Query to get the replies where parent is NULL for the given discussion ID
+//         const result = await db.query(
+//             'SELECT * FROM replies WHERE  parent_id = $1', 
+//             [parentId]
+//         );
+
+//         // Send the replies as a JSON response
+//         res.json(result.rows);
+//     } catch (err) {
+//         console.error('Error querying the database:', err);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// });
+
+
+
+
+passport.use(
+    "google",
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+      },
+      async (accessToken, refreshToken, profile, cb) => {
+        try {
+          // console.log(profile);
+          const result = await db.query("SELECT * FROM users WHERE email = $1", [
+            profile.email,
+          ]);
+          if (result.rows.length === 0) {
+            const newUser = await db.query(
+              "INSERT INTO users (email, password) VALUES ($1, $2)",
+              [profile.email, "google"]
+            );
+            return cb(null, newUser.rows[0]);
+          } else {
+            return cb(null, result.rows[0]);
+          }
+        } catch (err) {
+          return cb(err);
+        }
+      }
+    )
+  );
+  passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
+  
+  passport.deserializeUser((user, cb) => {
+    cb(null, user);
+  });
+  
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
---
+++ public/js/book.js
@@ -0,0 +1,168 @@
+async function loadReplies(discussId, parentId = null) {
+  try {
+    try {
+      console.log("Fetching replies...");
+      const response = await fetch(
+        `/get-replies?discuss_id=${discussId}&parent_id=${parentId}`
+      );
+      const replies = await response.json();
+
+      // selects the child div if parent exists else selects the main div to display the duscussions
+      // now in div say the parent div first we put the replies that we got fomr the db for parent==null
+      // then we select the repliesReply div to display the replies
+      const repliesDiv = document.getElementById(
+        parentId ? `repliesReply-${parentId}` : `replies-${discussId}`
+      );
+
+      if (replies.length > 0) {
+        // for each of the div that we selct we add a button that calls the loadreplies child by giving the discussionid and the current id(replies-id) as the parent id
+        
+        const replyList = replies
+        
+          .map(
+            (reply) => `
+                  <div style="margin-left: 20px; background-color: #F4F1EA;">
+                      <p><strong>User:</strong> ${reply.user_id}</p>
+                      <p>${reply.text}</p>
+                      
+
+                        
+            
+
+<input type="text" id="replyReplyText-${reply.id}" placeholder="Type your reply here">
+<button onclick="sendReply(${discussId}, ${reply.id})">Reply</button>
+
+
+
+
+
+                      <button onclick="loadReplies(${discussId}, ${reply.id})">Show Replies</button>
+
+                      <div id="repliesReply-${reply.id}" style="display: none;"></div>
+                  </div>
+              `
+          )
+          .join("")
+          ;
+        // console.log(replyList);
+
+        //     <div style="margin-left: 20px; background-color: #F4F1EA;">
+        //     <p><strong>User:</strong> 2</p>
+        //     <p>Holden is a complex character; he’s relatable yet frustrating.</p>
+        //     <button onclick="loadReplies(1, 1)">Show Replies</button>
+
+        //     <div id="repliesReply-1" style="display: none;"></div>
+        // </div>
+
+        // <div style="margin-left: 20px; background-color: #F4F1EA;">
+        //     <p><strong>User:</strong> 3</p>
+        //     <p>His contradictions make him real to me.</p>
+        //     <button onclick="loadReplies(1, 2)">Show Replies</button>
+
+        //     <div id="repliesReply-2" style="display: none;"></div>
+        // </div>
+
+        // <div style="margin-left: 20px; background-color: #F4F1EA;">
+        //     <p><strong>User:</strong> 4</p>
+        //     <p>I agree, he’s very well written.</p>
+        //     <button onclick="loadReplies(1, 3)">Show Replies</button>
+
+        //     <div id="repliesReply-3" style="display: none;"></div>
+        // </div>
+
+        // <div style="margin-left: 20px; background-color: #F4F1EA;">
+        //     <p><strong>User:</strong> 4</p>
+        //     <p>I agree, he’s very well written.</p>
+        //     <button onclick="loadReplies(1, 119)">Show Replies</button>
+
+        //     <div id="repliesReply-119" style="display: none;"></div>
+        // </div>
+
+        repliesDiv.innerHTML = replyList;
+        repliesDiv.style.display = "block";
+      } else {
+        // repliesDiv.innerHTML = "<p>No replies available.</p>";
+        // repliesDiv.style.display = "block";
+      }
+    } catch (error) {
+      console.error("Error fetching replies:", error);
+    }
+  } catch (error) {
+    console.error("Error fetching replies:", error);
+  }
+}
+
+async function sendDiscussion(book_id) {
+  try {
+    // Get the reply text from the input field
+    const replyText = document.getElementById("discussText").value;
+
+    // Ensure the reply text is not empty
+    if (!replyText.trim()) {
+      alert("Please enter a reply.");
+      return;
+    }
+
+    // Send the reply to the server
+    const response = await fetch(
+      `/post-discuss?book_id=${book_id}&text=${encodeURIComponent(replyText)}`,
+      {
+        method: "POST",
+      }
+    );
+
+    if (response.ok) {
+      alert("Reply posted successfully!");
+      // location.reload(); // Reload the page to show the new reply (optional)
+    } else {
+      alert("Failed to post the reply.");
+    }
+  } catch (error) {
+    console.error("Error posting reply:", error);
+  }
+}
+
+async function sendReply(discuss_id, parent_id = null) {
+  try {
+    // Get the reply text from the input field
+
+
+    
+    let replyText = document.getElementById(`replyText-${discuss_id}`).value;
+    if (parent_id!==null) {
+      replyText = document.getElementById(`replyReplyText-${parent_id}`).value;
+      console.log("its workinggg shayd")
+    }
+   
+
+    
+
+    console.log(`parent-id is ${parent_id}`);
+
+
+    // Ensure the reply text is not empty
+    if (!replyText.trim()) {
+      alert("Please enter a reply.");
+      return;
+    }
+
+    // Send the reply to the server
+    const response = await fetch(
+      `/post-reply?discuss_id=${discuss_id}&parent_id=${parent_id}&text=${encodeURIComponent(
+        replyText
+      )}`,
+      {
+        method: "POST",
+      }
+    );
+
+    if (response.ok) {
+      alert("Reply posted successfully!");
+      // location.reload(); // Reload the page to show the new reply (optional)
+    } else {
+      alert("Failed to post the reply.");
+    }
+  } catch (error) {
+    console.error("Error posting reply:", error);
+  }
+}
